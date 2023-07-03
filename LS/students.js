49c8e14488d95ee868346e76c5419c35ae394fd8
IGrizzz <muda3332@gmail.com>
2023-07-03 22:07:53 +0700

    remove trash

--- LS/students.js
+++
@@ -1,8 +0,0 @@
-const students = [
-    { name : "Syaipuddin", },
-    { name: "Rizky", age: 14},
-    { name: "Siraj", age: 17}
-]
-
-console.log(students[0]);
-console.log(`Hello, my name is ${students[0].name} and my banjar is ${students[0].city}`)
