9bb7e4f71d98deddbe8ed9bd6ffd17a0fb4ada91
IGrizzz <muda3332@gmail.com>
2023-07-03 21:57:43 +0700

    done withJs and noJs

---
+++ LS/students.js
@@ -0,0 +1,8 @@
+const students = [
+    { name : "Syaipuddin", },
+    { name: "Rizky", age: 14},
+    { name: "Siraj", age: 17}
+]
+
+console.log(students[0]);
+console.log(`Hello, my name is ${students[0].name} and my banjar is ${students[0].city}`)
